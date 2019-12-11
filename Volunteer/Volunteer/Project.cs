fd74a0e2c8985799fa8e997022b1807b145c20b5
filfel <filip.fellman@bambora.com>
2019-12-11 17:45:07 +0100

    adding first item to listview, hardcodede

--- Volunteer/Volunteer/Project.cs
+++ Volunteer/Volunteer/Project.cs
@@ -1,29 +1,19 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Collections.ObjectModel;
-//using System.Text;
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
 
-//namespace Volunteer
-//{
-//    class Project
-//    {
-//        public string DisplayProject { get; set; }
-//    }
+namespace Volunteer
+{
+    public class Project
+    {
+        public string Name { get; set; }
+        public string Location { get; set; }
+        public string ImageUrl { get; set; }
 
-//    ObservableCollection<Project> projects = new ObservableCollection<Project>();
-//    public ObservableCollection<Project> Projects { get { return projects; } }
-
-//    public ProjectListPage()
-//    {
-//        ProjecList.ItemsSource = projects;
-
-//        // ObservableCollection allows items to be added after ItemsSource
-//        // is set and the UI will react to changes
-//        employees.Add(new Employee { DisplayName = "Rob Finnerty" });
-//        employees.Add(new Employee { DisplayName = "Bill Wrestler" });
-//        employees.Add(new Employee { DisplayName = "Dr. Geri-Beth Hooper" });
-//        employees.Add(new Employee { DisplayName = "Dr. Keith Joyce-Purdy" });
-//        employees.Add(new Employee { DisplayName = "Sheri Spruce" });
-//        employees.Add(new Employee { DisplayName = "Burt Indybrick" });
-//    }
-//}
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
--- Volunteer/Volunteer/VolunteerPage.xaml.cs
+++ Volunteer/Volunteer/VolunteerPage.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,11 +11,23 @@ namespace Volunteer
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class VolunteerPage : ContentPage
     {
+        public IList<Project> Projects { get; private set; }
         public VolunteerPage()
 
         {
             InitializeComponent();
             Console.WriteLine("Volunteer Page reached!");
+
+            Projects = new List<Project>();
+            Projects.Add(new Project
+            {
+                Name = "Språk Café",
+                Location = "Rinkeby Bibliotek",
+                ImageUrl = "https://images.unsplash.com/photo-1507842217343-583bb7270b66?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80"
+
+            });
+
+            BindingContext = this;
         }
     }
 }
