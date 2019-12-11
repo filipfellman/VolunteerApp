512d737bddc3eb46dab0dd030aec96d16f479b3b
filfel <filip.fellman@bambora.com>
2019-12-11 21:19:29 +0100

    add hardcoded objects

--- Volunteer/Volunteer/VolunteerPage.xaml.cs
+++ Volunteer/Volunteer/VolunteerPage.xaml.cs
@@ -26,6 +26,48 @@ namespace Volunteer
                 ImageUrl = "https://images.unsplash.com/photo-1507842217343-583bb7270b66?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80"
 
             });
+            Projects.Add(new Project
+            {
+                Name = "Språk Café",
+                Location = "Rinkeby Bibliotek",
+                ImageUrl = "https://images.unsplash.com/photo-1507842217343-583bb7270b66?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80"
+
+            });
+            Projects.Add(new Project
+            {
+                Name = "Språk Café",
+                Location = "Rinkeby Bibliotek",
+                ImageUrl = "https://images.unsplash.com/photo-1507842217343-583bb7270b66?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80"
+
+            });
+            Projects.Add(new Project
+            {
+                Name = "Språk Café",
+                Location = "Rinkeby Bibliotek",
+                ImageUrl = "https://images.unsplash.com/photo-1507842217343-583bb7270b66?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80"
+
+            });
+            Projects.Add(new Project
+            {
+                Name = "Språk Café",
+                Location = "Rinkeby Bibliotek",
+                ImageUrl = "https://images.unsplash.com/photo-1507842217343-583bb7270b66?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80"
+
+            });
+            Projects.Add(new Project
+            {
+                Name = "Språk Café",
+                Location = "Rinkeby Bibliotek",
+                ImageUrl = "https://images.unsplash.com/photo-1507842217343-583bb7270b66?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80"
+
+            });
+            Projects.Add(new Project
+            {
+                Name = "Språk Café",
+                Location = "Rinkeby Bibliotek",
+                ImageUrl = "https://images.unsplash.com/photo-1507842217343-583bb7270b66?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80"
+
+            });
 
             BindingContext = this;
         }
