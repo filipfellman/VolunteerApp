e849436f8431f8c9583dd3f6d4e4bd9df69efb09
filfel <filip.fellman@bambora.com>
2019-12-12 21:43:15 +0100

    fixing random stuff

--- Volunteer/Volunteer/VolunteerPage.xaml.cs
+++ Volunteer/Volunteer/VolunteerPage.xaml.cs
@@ -68,7 +68,6 @@ namespace Volunteer
                 ImageUrl = "https://images.unsplash.com/photo-1507842217343-583bb7270b66?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80"
 
             });
-
             BindingContext = this;
         }
     }
