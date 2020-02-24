ee6c5d642cf262b58aa42a788f18a2e54775ef7e
filfel <filip.fellman@bambora.com>
2020-02-24 10:25:11 +0100

    added actual pages for login and register

--- Volunteer/Volunteer/MainPage.xaml.cs
+++ Volunteer/Volunteer/MainPage.xaml.cs
@@ -16,24 +16,24 @@ namespace Volunteer
         }
         //private NavigationPage navigationPage = new NavigationPage();
 
-        private void LoginClicked(object sender, EventArgs args)
+        private async void LoginClicked(object sender, EventArgs args)
         {
             try
             {
                 Console.WriteLine("LOGIN clicked!");
-                DisplayAlert("TBI", "To be implemented...", "Oh, okay...");
+                await Navigation.PushAsync(new LoginPage());
             }
             catch (Exception ex)
             {
                 throw ex;
             }
         }
-        private void RegisterClicked(object sender, EventArgs args)
+        private async void RegisterClicked(object sender, EventArgs args)
         {
             try
             {
                 Console.WriteLine("Registed clicked!");
-                DisplayAlert("TBI", "To be implemented...", "Oh, okay...");
+                await Navigation.PushAsync(new RegisterPage());
             }
             catch (Exception ex)
             {
@@ -41,30 +41,17 @@ namespace Volunteer
             }
         }
 
-       private void ContinueClicked(object sender, EventArgs args)
+       private async void ContinueClicked(object sender, EventArgs args)
         {
             try
             {
                 Console.WriteLine("Continue clicked!");
-                DisplayAlert("TBI", "To be implemented...", "Oh, okay...");
+                await Navigation.PushAsync(new VolunteerPage());
             }
             catch (Exception ex)
             {
                 throw ex;
             }
         }
-
-        private async void VolunteerClicked(object sender, EventArgs e)
-        {
-            await Navigation.PushAsync(new VolunteerPage());
-            Console.WriteLine("Volunteer clicked!");
-        }
-
-        private async void OrganizationClicked(object sender, EventArgs e)
-        {
-            Console.WriteLine("Voluntary Organization clicked!");
-            await Navigation.PushAsync(new OrganizationPage());
-        }
-
     }
 }
